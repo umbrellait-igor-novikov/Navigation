6baa996a7f406aba4e4256e16882ce1ce5b7ba67
Igor Novikov <igor.novikov@umbrellait.com>
2021-07-02 14:03:00 +0300

    Blank fragment has been added

---
+++ app/src/main/java/com/example/navigation/view_pager/ViewPagerContainerFragment.kt
@@ -0,0 +1,14 @@
+package com.example.navigation.view_pager
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.example.navigation.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+class ViewPagerContainerFragment : Fragment(R.layout.fragment_container_view_pager)
