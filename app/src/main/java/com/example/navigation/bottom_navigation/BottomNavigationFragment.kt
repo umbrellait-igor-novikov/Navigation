d0fa0c9106ed1e6112e844848a1c37a20c5e2495
Igor Novikov <igor.novikov@umbrellait.com>
2021-07-02 11:30:56 +0300

    Content fragments and bottom navigation listener have been added

--- app/src/main/java/com/example/navigation/bottom_navigation/BottomNavigationFragment.kt
+++ app/src/main/java/com/example/navigation/bottom_navigation/BottomNavigationFragment.kt
@@ -3,15 +3,38 @@ package com.example.navigation.bottom_navigation
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import com.example.navigation.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class BottomNavigationFragment : Fragment(R.layout.fragment_bottom_navigation) {
+    lateinit var bottomNavigation:BottomNavigationView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        bottomNavigation = view!!.findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.to_home -> {
+                    true
+                }
+                R.id.squares -> {
+                    true
+                }
+                R.id.notification -> {
+                    true
+                }
+                else -> false
+            }
+        }
+        return view
     }
 
 }
