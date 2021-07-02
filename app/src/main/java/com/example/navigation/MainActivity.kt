fd9865ea02c8e87ff131fc0dadbbfae99c72252f
Igor Novikov <igor.novikov@umbrellait.com>
2021-07-02 16:01:15 +0300

    NavigationDrawer sheet has been added

--- app/src/main/java/com/example/navigation/MainActivity.kt
+++ app/src/main/java/com/example/navigation/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.navigation
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.navigation.bottom_sheet.BottomSheetContainerFragment
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.navigation.view_pager.ViewPagerContainerFragment
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val drawerLayout:DrawerLayout = findViewById(R.id.drawer_layout)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .add(R.id.host_fragment, ViewPagerContainerFragment()).commit()
