5fab73d215262e1caa273fdc13a7443ba35fece4
Igor Novikov <igor.novikov@umbrellait.com>
2021-07-02 11:42:11 +0300

    Navigation between bottom fragments has been added

--- app/src/main/java/com/example/navigation/bottom_navigation/BottomNavigationFragment.kt
+++ app/src/main/java/com/example/navigation/bottom_navigation/BottomNavigationFragment.kt
@@ -7,11 +7,14 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import com.example.navigation.R
+import com.example.navigation.bottom_navigation.content_fragments.BottomNavigationContentFragment1
+import com.example.navigation.bottom_navigation.content_fragments.BottomNavigationContentFragment2
+import com.example.navigation.bottom_navigation.content_fragments.BottomNavigationContentFragment3
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class BottomNavigationFragment : Fragment(R.layout.fragment_bottom_navigation) {
-    lateinit var bottomNavigation:BottomNavigationView
+    lateinit var bottomNavigation: BottomNavigationView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,16 +22,33 @@ class BottomNavigationFragment : Fragment(R.layout.fragment_bottom_navigation) {
         savedInstanceState: Bundle?
     ): View? {
         val view = super.onCreateView(inflater, container, savedInstanceState)
+        parentFragmentManager.beginTransaction().add(
+            R.id.bottom_navigation_fragment_content,
+            BottomNavigationContentFragment1()
+        ).commit()
+
         bottomNavigation = view!!.findViewById(R.id.bottom_navigation)
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.to_home -> {
+                    parentFragmentManager.beginTransaction().replace(
+                        R.id.bottom_navigation_fragment_content,
+                        BottomNavigationContentFragment1()
+                    ).commit()
                     true
                 }
                 R.id.squares -> {
+                    parentFragmentManager.beginTransaction().replace(
+                        R.id.bottom_navigation_fragment_content,
+                        BottomNavigationContentFragment2()
+                    ).commit()
                     true
                 }
                 R.id.notification -> {
+                    parentFragmentManager.beginTransaction().replace(
+                        R.id.bottom_navigation_fragment_content,
+                        BottomNavigationContentFragment3()
+                    ).commit()
                     true
                 }
                 else -> false
