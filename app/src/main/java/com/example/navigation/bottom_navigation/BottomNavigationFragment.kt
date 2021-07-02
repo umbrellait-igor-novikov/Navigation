d2249f6aebc2e8f19f7a799aa6e1ed512c658b85
Igor Novikov <igor.novikov@umbrellait.com>
2021-07-02 10:28:01 +0300

    Blank fragment was added

---
+++ app/src/main/java/com/example/navigation/bottom_navigation/BottomNavigationFragment.kt
@@ -0,0 +1,17 @@
+package com.example.navigation.bottom_navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.navigation.R
+
+
+class BottomNavigationFragment : Fragment(R.layout.fragment_bottom_navigation) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
