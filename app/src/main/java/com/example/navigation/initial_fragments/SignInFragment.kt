e112370d369020d81e36cb3235eab2811655678f
Igor Novikov <igor.novikov@umbrellait.com>
2021-07-02 17:08:15 +0300

    Navigation from initial fragments to BottomNavigationContainerFragment

--- app/src/main/java/com/example/navigation/initial_fragments/SignInFragment.kt
+++ app/src/main/java/com/example/navigation/initial_fragments/SignInFragment.kt
@@ -1,6 +1,18 @@
 package com.example.navigation.initial_fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.navigation.R
+import com.example.navigation.bottom_navigation.BottomNavigationContainerFragment
 
-class SignInFragment : Fragment(R.layout.fragment_sign_in)
+class SignInFragment : Fragment(R.layout.fragment_sign_in){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val signInToMainButton = view.findViewById<Button>(R.id.button_from_sign_in_to_main)
+        signInToMainButton.setOnClickListener {
+            parentFragmentManager.beginTransaction().replace(R.id.host_fragment, BottomNavigationContainerFragment()).addToBackStack(null).commit()
+        }
+    }
+}
--- app/src/main/java/com/example/navigation/initial_fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/navigation/initial_fragments/SignUpFragment.kt
@@ -1,6 +1,17 @@
 package com.example.navigation.initial_fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.navigation.R
 
-class SignUpFragment : Fragment(R.layout.fragment_sign_up)
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val signUpToSignInButton = view.findViewById<Button>(R.id.button_from_sign_up_to_sign_in)
+        signUpToSignInButton.setOnClickListener {
+            parentFragmentManager.beginTransaction().replace(R.id.host_fragment, SignInFragment()).addToBackStack(null).commit()
+        }
+    }
+}
